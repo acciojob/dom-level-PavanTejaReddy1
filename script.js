f34158025d74cea2a3f904cd15eb8840f2dc627f
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-01 21:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let level = document.getElementById("level");
+
+let count = 0;
+let currentLevel = level;
+
+while (currentLevel != null) {
+	currentLevel = currentLevel.parentNode;
+	count++
+}
+
+alert("The level of the element is: " + count);
